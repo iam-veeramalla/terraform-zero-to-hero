b9376218312d34883ff0bcf121a23143afd91b46
Abhishek Veeramalla <abhishek.veeramalla@gmail.com>
2023-09-14 20:06:46 +0530

    Added files for Day-3

---
+++ Day-3/modules/ec2_instance/variables.tf
@@ -0,0 +1,11 @@
+variable "ami_value" {
+    description = "value for the ami"
+}
+
+variable "instance_type_value" {
+    description = "value for instance_type"
+}
+
+variable "subnet_id_value" {
+    description = "value for the subnet_id"
+}
