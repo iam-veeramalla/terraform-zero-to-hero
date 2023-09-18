60afef64a0f8eebdacaa4a4362a01981765f9558
OOluomo <111322222+OOluomo@users.noreply.github.com>
2023-09-18 16:41:42 -0500

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-053b0d53c279acc90"
     instance_type = "t2.micro"
 }
