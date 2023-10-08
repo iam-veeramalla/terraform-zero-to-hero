ced262e0ad5ab9552b770967c26756cfd12bd7e0
basantgit <basantmohanty7@gmail.com>
2023-10-08 20:16:49 +0000

    second commit

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
+    Name  "terraformtestserver"
     ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
