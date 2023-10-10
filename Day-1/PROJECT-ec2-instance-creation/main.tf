1033e8a47ae7bb665bbbe4f40a8c53140448040f
Amruthachaluvadi <amruthachaluvadi@gmail.com>
2023-10-10 23:02:13 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-036f5574583e16426"  # Specify an appropriate AMI ID
+   ami           = "ami-036f5574583e16426"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = "subnet-0bd8db4ecd210d65b"
+    key_name = "terraform"
 }
