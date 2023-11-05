9b1357997db7ea2517e04a3d4d2b5a764312278e
Gopi <krisgopi107@gmail.com>
2023-11-05 10:34:03 +0000

    commiting the changes

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-0fc5d935ebf8bc3bc"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    key_name      = "terraform-key"
 }
