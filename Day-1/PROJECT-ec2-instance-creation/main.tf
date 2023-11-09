4a10759c5450785b5a919a22271fdd66a7ebaaf7
Gopi <krisgopi107@gmail.com>
2023-11-09 07:09:57 +0000

    updating the changes

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,9 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+  region = "us-east-1" # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0fc5d935ebf8bc3bc"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+  ami           = "ami-0fc5d935ebf8bc3bc" # Specify an appropriate AMI ID
+  instance_type = "t2.micro"
+  key_name      = "Terraform_test_keypair"
 }
