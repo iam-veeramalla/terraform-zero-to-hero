912ad19911e319e81936f4f24aa0a5df7d0d5d02
Gopi <krisgopi107@gmail.com>
2023-11-18 14:01:08 +0000

    updating the changes

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -6,4 +6,11 @@ resource "aws_instance" "example" {
   ami           = "ami-0fc5d935ebf8bc3bc" # Specify an appropriate AMI ID
   instance_type = "t2.micro"
   key_name      = "Terraform_test_keypair"
+
+  tags = {
+    Name             = "test-application"
+    Environment      = "dev"
+    Product_Code     = "dev-team"
+    team_leavel_name = "sweden"
+  }
 }
