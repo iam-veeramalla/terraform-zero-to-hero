1669fb4633bca2cc0407cc93cabddcf61347e7e8
ravitejachikkala <79794643+ravitejachikkala@users.noreply.github.com>
2024-02-22 20:08:32 +0530

    Update main.tf

--- Day-7/main.tf
+++ Day-7/main.tf
@@ -1,22 +1,22 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 provider "vault" {
-  address = "<>:8200"
+  address = "65.0.85.106:8200"
   skip_child_token = true
 
   auth_login {
     path = "auth/approle/login"
 
     parameters = {
-      role_id = "<>"
-      secret_id = "<>"
+      role_id = "4c53727e-4d09-bb45-7ad0-c4816dc8bd7b"
+      secret_id = "0c9c84b6-beea-39a8-9865-85d11ff595a8"
     }
   }
 }
 
-data "vault_kv_secret_v2" "example" {
+/*data "vault_kv_secret_v2" "example" {
   mount = "secret" // change it according to your mount
   name  = "test-secret" // change it according to your secret
 }
@@ -29,4 +29,4 @@ resource "aws_instance" "my_instance" {
     Name = "test"
     Secret = data.vault_kv_secret_v2.example.data["foo"]
   }
-}
+}*/
