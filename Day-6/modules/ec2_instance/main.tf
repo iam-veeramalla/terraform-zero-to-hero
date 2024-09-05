7b41fd3605dbf8dffc2bfb6505785eca844243d6
Shivam Pawar <147264094+shivampawar183@users.noreply.github.com>
2024-09-05 22:56:06 +0530

    changes to the variable names

--- Day-6/modules/ec2_instance/main.tf
+++ Day-6/modules/ec2_instance/main.tf
@@ -2,15 +2,15 @@ provider "aws" {
     region = "us-east-1"
 }
 
-variable "ami" {
+variable "ami_id" {
   description = "This is AMI for the instance"
 }
 
-variable "instance_type" {
+variable "instance_of_type" {
   description = "This is the instance type, for example: t2.micro"
 }
 
 resource "aws_instance" "example" {
-    ami = var.ami
-    instance_type = var.instance_type
+    ami = var.ami_id
+    instance_type = var.instance_of_type
 }
