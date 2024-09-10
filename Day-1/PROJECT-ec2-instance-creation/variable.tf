f73d5e2a267c928590b7d83adbe1a1c9e41b567e
Sasibeeke <42212652+Sasibeeke@users.noreply.github.com>
2024-09-10 04:57:58 +0000

    terraform simple e2 resource creation

---
+++ Day-1/PROJECT-ec2-instance-creation/variable.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  description = "value"
+  type = string
+  default = "us-east-1"
+}
+variable "instance_type" {
+  description = "value"
+  type = string
+  default = "t2.micro"
+}
+
+variable "ami" {
+  description = "value"
+  type = string
+  default = "ami-0e86e20dae9224db8"
+}
