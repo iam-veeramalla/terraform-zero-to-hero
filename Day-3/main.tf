ee52b14b1da85e58cb0a1eea8e9fcbf48e7881ec
deepakbangar87 <167755262+deepakbangar87@users.noreply.github.com>
2024-09-25 14:43:58 +0530

    Update main.tf

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "ec2_instance" {
   source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
+  ami_value = "ami-0e86e20dae9224db8" # replace this
   instance_type_value = "t2.micro"
   subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
 }
