9880ff167b0794f004fbce1636e459b6926cad8d
gpardes2 <ganeshmanojpardeshi@gmail.com>
2024-11-19 19:11:35 +0530

    Added

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -2,9 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "ec2_instance" {
-  source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
-  instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+resource "aws_instance" "example" {
+  ami = var.ami_value
+  instance_type = var.instance_type_value
+  subnet_id = var.subnet_id_value
 }
---
+++ Day-3/variables.tf
@@ -0,0 +1,13 @@
+variable "ami_value" {
+  description = " Value for the AMI"
+}
+
+variable "instance_type_value" {
+  description = "Instance Type value"
+}
+
+
+variable "subnet_id_value" {
+  description = "Value of Subnet ID"
+}
+
