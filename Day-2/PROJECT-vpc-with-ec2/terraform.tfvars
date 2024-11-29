e1dd5259ed82598eb8c94452107e3a8da41ee75c
abhi2k19 <abhijithas1999@gmail.com>
2024-11-29 12:33:37 +0530

    Updated tfvars, variables.tf, main.tf

--- Day-2/PROJECT-vpc-with-ec2/terraform.tfvars
+++ Day-2/PROJECT-vpc-with-ec2/terraform.tfvars
@@ -1,1 +1,2 @@
 ami_id = "ami-0261755bbcb8c4a84"
+instance_type = "t2.micro"
--- Day-2/PROJECT-vpc-with-ec2/variables.tf
+++ Day-2/PROJECT-vpc-with-ec2/variables.tf
@@ -5,3 +5,7 @@ variable "ami_id" {
   description = "EC2 AMI Id"
   type = string
 }
+variable "instance_type" {
+   description = "EC2 Instance Type"
+  type = string
+}
