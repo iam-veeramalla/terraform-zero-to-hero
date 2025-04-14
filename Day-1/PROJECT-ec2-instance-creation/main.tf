e3f1f22d87aaf5a2f7f14a46fa5c35d3e65723b3
Sai Sasidhar <saisasidhar@saisasidhar-MacBook-Pro-M1.local>
2025-04-14 12:38:15 +0530

    test

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,14 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "us-west-1"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+  ami           = "ami-0657605d763ac72a8"  # Specify an appropriate AMI ID
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "test-function"
+    Team = "test"
+    Billing = "test"
+  }
 }
