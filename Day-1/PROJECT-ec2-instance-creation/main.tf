6f931553cc274fda7ad501a7d99606ffa1219181
Sradha85 <sradhanjalir85@gmail.com>
2025-04-26 05:34:29 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+resource "aws_instance" "test" {
+    ami           = "ami-084568db4383264d4"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
