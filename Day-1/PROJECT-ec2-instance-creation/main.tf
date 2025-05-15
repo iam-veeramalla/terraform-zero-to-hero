a6e3506696d62e00cd78425ddb24aa6d2a44d8eb
shahistp <pirjadeshahistha@gmail.com>
2025-05-15 12:56:38 -0500

    comment added testing file for test

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID, testing file
     instance_type = "t3.micro"
 }
